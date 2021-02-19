b5e75557bfdaa91cb0fc53eea04a30b7381a3d3b
James <Thylinguistics@gmail.com>
2021-02-18 17:30:58 -0800

    initial

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,14 @@
+// for routes with id's
+// make dir inside pages, called this one article
+// make dir inside article called [id]
+// make file inside [id] called index.js....changed the name from index to article
+
+const article = () => {
+    return (
+        <div>
+            this is an article
+        </div>
+    )
+}
+
+export default article
